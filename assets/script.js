e4d1dffb088edefe039f80c0435867854ae8d612
sihalohoapry <madonaapry@gmail.com>
2021-06-29 21:08:02 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+
+const txtElement = ["Welcome to my portfolio, enjoy"];
+let count = 0;
+let txtIndex = 0;
+let currentText = "";
+let word = "";
+
+(function ngetik() {
+  if (count == txtElement.length) {
+    count = 0;
+  }
+  currentText = txtElement[count];
+  word = currentText.slice(0, ++txtIndex);
+  document.querySelector(".efekNgetik").textContent = word;
+
+  setTimeout(ngetik, 200);
+})();
